573ea034c4e18bc136fb75f3d5018a12027cb829
Chaithanya I <54620219+chaithanyai311@users.noreply.github.com>
2023-06-29 10:43:30 +0530

    Add files via upload

---
+++ api/db/models/index.js
@@ -0,0 +1,11 @@
+const { TeammemberList } = require('./teammember.model');
+const { TeamList } = require('./team.model');
+const { ProjectList} = require('./project.model');
+const { TaskList} = require('./task.model');
+
+module.exports = {
+   TeammemberList,
+   TeamList,
+   ProjectList,
+   TaskList
+}
